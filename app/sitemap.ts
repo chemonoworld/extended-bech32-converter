d8577e05b941fa5166862df33a2c054f7388f3c0
Calvin Lee <jinwoo0451@naver.com>
2024-04-29 01:13:32 +0900

    Modify sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://bech32converter.com',
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: 'daily',
       priority: 1,
     },
   ]
